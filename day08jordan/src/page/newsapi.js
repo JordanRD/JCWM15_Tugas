20c4cf4de1a6d35d73e5d50348d47f3e8c99624c
JordanRD <jordan.rafelino@gmail.com>
2020-11-28 11:08:36 +0700

    ok

--- day08jordan/src/page/newsapi.js
+++ day08jordan/src/page/newsapi.js
@@ -31,56 +31,38 @@ class News extends React.Component {
                 ['Rusia', 'ru'],
                 ['United States', 'us']
             ],
-            news: [
-                {
-                    title: 'gawat merokok dapat menghasilkan polusi',
-                    description: 'pak andi perokok ...'
-                },
-                {
-                    title: 'partai kacang polong diprotes warga',
-                    description: 'ibu siti memprotes karena...'
-                },
-                {
-                    title: 'narkoba semakin berbahaya',
-                    description: 'ayah dari seorang bocah memberi...'
-                },
-                {
-                    title: 'jangan berikan rokok',
-                    description: 'bocah memberi sapi...'
-                },
-                {
-                    title: 'andono bergabung dengan komunitas',
-                    description :'tapi ternyata...'
-                },
-                {
-                    title: 'presiden thailand setuju dengan anaknya',
-                    description :'setuju kalau anaknya...'
-                }
-            
-            ],
+            news: [],
             inputvalue: ''
+            
         }
     }
     componentDidMount() {
-        Axios.get(this.state.link[0])
-            .then((res) => {
-                this.setState({ news: res.data.articles })
+        
+         this.getdata()
 
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+    }
+    getdata = () => {
+        Axios.get(this.state.link[0])
+        .then((res) => {
+            this.setState({ news: res.data.articles })
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
     }
     showCard = () => {
-        console.log(this.state.link)
+
+        // console.log(this.state.link)
 
         let filternews = this.state.news.filter((a) => {
             let regex = new RegExp(this.state.inputvalue,'gi')
             if(regex.test(a.title))return a.title
         })
 
-        console.log(filternews);
-        console.log('ini '+this.state.inputvalue);
+        // console.log(filternews);
+        // console.log('ini '+this.state.inputvalue);
         return filternews.map((item, index) => {
             return (
                 <Card style={{ width: '18rem' }}>
@@ -108,14 +90,16 @@ class News extends React.Component {
         let jumlah = URL + this.state.country[event][1] + KEY
         let temp = [jumlah]
         this.setState({ link: temp })
-
         Axios.get(this.state.link[0])
-            .then((res) => {
-                return this.setState({ news: res.data.articles })
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+        .then((res) => {
+            this.setState({ news: res.data.articles })
+            this.getdata()
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
     }
 
     Input = (i) => {
@@ -124,7 +108,7 @@ class News extends React.Component {
     }
 
     render() {
-        console.log(this.state.link);
+        // console.log(this.state.link);
         return (
             <div >
                 <Navbar bg="light" expand="lg" style={styles.h1props}>
@@ -170,4 +154,27 @@ export default News
 
 
 
-
+                // {
+                //     title: 'gawat merokok dapat menghasilkan polusi',
+                //     description: 'pak andi perokok ...'
+                // },
+                // {
+                //     title: 'partai kacang polong diprotes warga',
+                //     description: 'ibu siti memprotes karena...'
+                // },
+                // {
+                //     title: 'narkoba semakin berbahaya',
+                //     description: 'ayah dari seorang bocah memberi...'
+                // },
+                // {
+                //     title: 'jangan berikan rokok',
+                //     description: 'bocah memberi sapi...'
+                // },
+                // {
+                //     title: 'andono bergabung dengan komunitas',
+                //     description :'tapi ternyata...'
+                // },
+                // {
+                //     title: 'presiden thailand setuju dengan anaknya',
+                //     description :'setuju kalau anaknya...'
+                // }
